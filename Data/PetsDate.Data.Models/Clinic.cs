e88a494b953da5d0cb13451a75b7352cd246ecf2
andreivorona <andreivorona@abv.bg>
2020-11-17 13:04:04 +0200

    Add Tables: Clinics and ClinicImages.And add migrations.

---
+++ Data/PetsDate.Data.Models/Clinic.cs
@@ -0,0 +1,26 @@
+﻿namespace PetsDate.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using PetsDate.Data.Common.Models;
+
+    public class Clinic : BaseDeletableModel<string>
+    {
+        public Clinic()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.ClinicImages = new HashSet<ClinicImage>();
+        }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public ICollection<ClinicImage> ClinicImages { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
--- Data/PetsDate.Data/ApplicationDbContext.cs
+++ Data/PetsDate.Data/ApplicationDbContext.cs
@@ -30,6 +30,10 @@
 
         public DbSet<Image> Images { get; set; }
 
+        public DbSet<Clinic> Clinics { get; set; }
+
+        public DbSet<ClinicImage> ClinicImages { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
