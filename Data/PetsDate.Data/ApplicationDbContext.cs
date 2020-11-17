3f7d226f5de612444629330c156b1077f638187c
andreivorona <andreivorona@abv.bg>
2020-11-17 13:48:55 +0200

    Add table Publications.And add Migration.

--- Data/PetsDate.Data/ApplicationDbContext.cs
+++ Data/PetsDate.Data/ApplicationDbContext.cs
@@ -34,6 +34,8 @@
 
         public DbSet<ClinicImage> ClinicImages { get; set; }
 
+        public DbSet<Publication> Publications { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
---
+++ Data/PetsDate.Data/Migrations/20201117113731_addTablePublications.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetsDate.Data.Migrations
+{
+    public partial class addTablePublications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Publications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Publications_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publications_IsDeleted",
+                table: "Publications",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publications_UserId",
+                table: "Publications",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publications");
+        }
+    }
+}
