faf1aef0b61f509a3b76a290d38b6f599e59a8c1
andreivorona <andreivorona@abv.bg>
2020-11-20 12:02:55 +0200

    Add form and validation for Animal.

--- Web/PetsDate.Web.ViewModels/Animal/CreateAnimalInputModel.cs
+++ Web/PetsDate.Web.ViewModels/Animal/CreateAnimalInputModel.cs
@@ -1,7 +1,25 @@
 ﻿namespace PetsDate.Web.ViewModels.Animal
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateAnimalInputModel
     {
+        [MinLength(2)]
+        [Required]
+        public string Name { get; set; }
 
+        [Required]
+        [Range(1, 400)]
+        public int Age { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Color { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Weight { get; set; }
+
+        public int CategoryId { get; set; }
     }
 }
